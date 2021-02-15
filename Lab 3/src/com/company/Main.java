bbc22abc78eb0d4ff2c592e0c26cc41fa3dbe042
Ksuvot <ksuvot36@yandex.ru>
2021-02-15 14:17:23 +0300

    add Lab 3

---
+++ Lab 3/src/com/company/Main.java
@@ -0,0 +1,61 @@
+//  Copyright © 2021 Ksuvot
+package com.company;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Dear user, please, enter the path to your .txt file:");
+        Scanner scanner = new Scanner(System.in);
+        String path = scanner.nextLine();
+        scanner.close();
+        readFile(path);
+    }
+
+    static void readFile(String path) {
+        try(FileReader reader = new FileReader(path)) {
+            Scanner scanner = new Scanner(reader);
+            short[] alphabet = new short[26];
+
+            while (scanner.hasNextLine()) {
+                String string = scanner.nextLine();
+                char[] chars = string.toCharArray();
+                for (char symbol : chars) {
+                   if ((int)symbol >= 65 && (int)symbol <= 90) { // a ... z
+                       ++alphabet[(int)symbol - 65];
+                   } else if ((int)symbol >= 97 && (int)symbol <= 122) { // A ... Z
+                       ++alphabet[(int)symbol - 97];
+                   }
+                }
+            }
+
+            reader.close();
+            scanner.close();
+
+            writeFile(alphabet);
+        }
+        catch(IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    static void writeFile(short[] alphabet) {
+        Path path = Paths.get("src\\resources\\output.txt").toAbsolutePath();
+        try(FileWriter writer = new FileWriter(path.toString())) {
+            for (short i = 0; i < 26; i++) {
+                writer.append((char)(65 + i) + " " + (char)(97 + i) + " - " + alphabet[i] + "\n");
+            }
+            writer.close();
+            System.out.println("Writing to the file was successful!");
+        }
+        catch(IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+}
