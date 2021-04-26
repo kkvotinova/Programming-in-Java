8b69b3a1466355e589182f6bc6ec0ba2ab4887d5
Ksuvot <ksuvot36@yandex.ru>
2021-04-26 11:46:28 +0300

    Lab 5 is done

--- Lab 5/src/com/company/Main.java
+++ Lab 5/src/com/company/Main.java
@@ -1,8 +1,35 @@
+/*
+ * Copyright (c) 2021 Ksuvot
+ */
+
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("{ Enter the following information }");
+
+        System.out.print("[ number of elevators ] ");
+        int numberOfElevators = Integer.parseInt(scanner.nextLine());
+        System.out.print("[ elevator capacity ] ");
+        int elevatorCapacity = Integer.parseInt(scanner.nextLine());
+        System.out.print("[ number of floors ] ");
+        int numberOfFloors = Integer.parseInt(scanner.nextLine());
+        System.out.print("[ maximum number of people per floor ] ");
+        int maxNumberOfPeoplePerFloor = Integer.parseInt(scanner.nextLine());
+
+        if (numberOfFloors * elevatorCapacity * numberOfElevators * maxNumberOfPeoplePerFloor > 0) {
+            ElevatorControl elevatorControl = new ElevatorControl(elevatorCapacity, numberOfFloors, numberOfElevators);
+            Request request = new Request(numberOfFloors, maxNumberOfPeoplePerFloor, elevatorControl);
+            Thread requestsThread = new Thread(request);
+            Thread elevatorsThread = new Thread(elevatorControl);
+            requestsThread.start();
+            elevatorsThread.start();
+        }
+        else {
+            System.out.print("❌ WRONG ❌");
+        }
     }
 }
---
+++ Lab 5/src/com/company/Request.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2021 Ksuvot
+ */
+
+package com.company;
+
+import java.util.Random;
+
+public class Request implements Runnable {
+    private int currentFloor;
+    private int maxPeopleOnFloor;
+    private ElevatorControl elevatorControl;
+
+    Request (int currentFloor, int maxPeopleOnFloor, ElevatorControl elevatorControl) {
+        setCurrentFloor(currentFloor);
+        setMaxPeopleOnFloor(maxPeopleOnFloor);
+        setElevatorControl(elevatorControl);
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            Random random = new Random();
+            int currentFloor = random.nextInt(this.currentFloor + 1);
+            int numberOfPeople = random.nextInt(maxPeopleOnFloor + 1);
+            for (int i = 0; i < numberOfPeople; i++) {
+                int nextFloor = random.nextInt(this.currentFloor + 1);
+                while (nextFloor == currentFloor) {
+                    nextFloor = random.nextInt(this.currentFloor + 1);
+                }
+                getElevatorControl().setListOfPassengers(new Passenger(currentFloor, nextFloor));
+            }
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    public void setCurrentFloor(int currentFloor) {
+        this.currentFloor = currentFloor;
+    }
+
+    public void setMaxPeopleOnFloor(int maxPeopleOnFloor) {
+        this.maxPeopleOnFloor = maxPeopleOnFloor;
+    }
+
+    public ElevatorControl getElevatorControl() {
+        return elevatorControl;
+    }
+
+    public void setElevatorControl(ElevatorControl elevatorControl) {
+        this.elevatorControl = elevatorControl;
+    }
+}
