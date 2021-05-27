e84880265430292a3d3c36199055759cda56323e
Ksuvot <ksuvot36@yandex.ru>
2021-05-27 18:46:32 +0300

    add Main.Java

---
+++ Lab 5/src/com/company/Main.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2021 Ksuvot
+ */
+
+package com.company;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("{ Enter the following information }");
+
+        System.out.print("[ number of elevators ] ");
+        int numberOfElevators = Integer.parseInt(scanner.nextLine());
+        System.out.print("[ elevator capacity ] ");
+        int elevatorCapacity = Integer.parseInt(scanner.nextLine());
+        System.out.print("[ number of floors ] ");
+        int numberOfFloors = Integer.parseInt(scanner.nextLine());
+        System.out.print("[ maximum number of people per floor ] ");
+        int maxNumberOfPeoplePerFloor = Integer.parseInt(scanner.nextLine());
+
+        if (numberOfFloors * elevatorCapacity * numberOfElevators * maxNumberOfPeoplePerFloor > 0) {
+            ElevatorControl elevatorControl = new ElevatorControl(elevatorCapacity, numberOfFloors, numberOfElevators);
+            Request request = new Request(numberOfFloors, maxNumberOfPeoplePerFloor, elevatorControl);
+            Thread requestsThread = new Thread(request);
+            Thread elevatorsThread = new Thread(elevatorControl);
+            requestsThread.start();
+            elevatorsThread.start();
+        }
+        else {
+            System.out.print("❌ WRONG ❌");
+        }
+    }
+}
